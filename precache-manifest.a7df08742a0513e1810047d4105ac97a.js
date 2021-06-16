4958ba9b85e6b2a90673eec2a988a192f0ce0c1f
Code Sandbox <hello@codesandbox.io>
2021-06-16 15:09:05 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a7df08742a0513e1810047d4105ac97a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af49f020fc43706a30ad929a229faea0",
+    "url": "https://igiboi.github.io/csb-59x3c/index.html"
+  },
+  {
+    "revision": "6b5c8dd48ab96169f50e",
+    "url": "https://igiboi.github.io/csb-59x3c/static/js/2.ab7a44ae.chunk.js"
+  },
+  {
+    "revision": "28c8379b155df50cb54f",
+    "url": "https://igiboi.github.io/csb-59x3c/static/js/main.9f44d01c.chunk.js"
+  },
+  {
+    "revision": "25494f472da8cf9aef2b",
+    "url": "https://igiboi.github.io/csb-59x3c/static/js/runtime-main.0ab2f12e.js"
+  }
+]);
